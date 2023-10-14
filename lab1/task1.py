e9d9e09eb51d2f6ed6bbf17467257f7434a15444
yrmint <90215333+yrmint@users.noreply.github.com>
2023-10-14 13:18:43 +0300

    Update task1.py

--- lab1/task1.py
+++ lab1/task1.py
@@ -1,10 +1,7 @@
 numbers = [2, -93, -2, 8, None, -44, -1, -85, -14, 90, -22, -90, -100, -8, 38, -92, -45, 67, 53, 25]
 
-nums = numbers[:]
-nums.pop(numbers.index(None))
-
-ave = sum(nums) / len(numbers)
-
-numbers[numbers.index(None)] = ave
+ind = numbers.index(None)
+numbers.pop(ind)
+numbers.insert(ind, sum(numbers) / (len(numbers) + 1))
 
 print("Измененный список:", numbers)
