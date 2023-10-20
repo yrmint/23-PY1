a2b6b2b6ad29899dbeeaf8bff7db6af0e83f869a
yrmint <90215333+yrmint@users.noreply.github.com>
2023-10-21 00:40:13 +0300

    lab2

---
+++ lab2/task1.py
@@ -0,0 +1,17 @@
+money_capital = 20000  # Подушка безопасности
+salary = 5000  # Ежемесячная зарплата
+spend = 6000  # Траты за первый месяц
+increase = 0.05  # Ежемесячный рост цен
+
+months = 0  # Количество месяцев, которое можно протянуть без долгов
+
+while True:
+    diff = spend - salary
+    if diff > money_capital:
+        break
+
+    months += 1
+    money_capital -= diff
+    spend *= 1 + increase
+
+print("Количество месяцев, которое можно протянуть без долгов:", months)
